c593a95af51609bc0f2fa3beb5f6c2388f1eaaff
Luan <luanmarques1789@gmail.com>
2021-09-13 22:36:05 -0300

    Conditional expression

---
+++ conditional-expression.py
@@ -0,0 +1,16 @@
+
+time = int(input('How much time does your car have? '))
+
+# SHORT HAND
+print("It's new" if time <= 3 else "It's old")
+
+# "This technique is known as Ternary Operators, or Conditional Expressions."
+# by www.w3schools.com
+
+# LONG HAND
+# if (time <= 3):
+#     print("It's new")
+# else:
+#     print("It's old")
+
+print(f'{"END":-^10}')
