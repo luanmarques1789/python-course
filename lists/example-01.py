5850edca0d1fcc6edd79527673b1aa3d62c77ff0
Luan M. Marques <luanmarques1789@gmail.com>
2021-09-19 15:06:48 -0300

    list example 01

---
+++ lists/example-01.py
@@ -0,0 +1,8 @@
+name = str(input('Type some name: ')).strip().lower()
+
+list = ['luan', 'edna', 'gustavo', 'miguel', 'ana']
+
+if (name in list):
+    print('Especial name!')
+else:
+    print('Normal name...')
