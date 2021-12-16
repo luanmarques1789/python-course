4d7ead6aec652e3c2b65bcf51f89b6138ae2dc7b
Luan M. Marques <luanmarques1789@gmail.com>
2021-12-16 16:23:30 -0300

    link video and comments added

--- decorators/decorator.py
+++ decorators/decorator.py
@@ -1,3 +1,5 @@
+# video: https://www.youtube.com/watch?v=P0aW1czXHio
+
 import requests
 import time
 
@@ -6,15 +8,17 @@ def calc_time(func):
     """Calculate the runtime of the function passed in the parameter"""
     def wrapper():
         init_time = time.time()
-        print('Getting the exchange time...')
+        print('Getting the time...')
         func()
         final_time = time.time()
-        print(f'The exchange time is: {final_time - init_time:.2f} s')
+        print(f'The response time was: {final_time - init_time:.2f} s')
     return wrapper
 
 
 @ calc_time
 def get_dollar_exchange():
+    """Get the dollar exchange rate from the API"""
+
     url = 'https://economia.awesomeapi.com.br/last/USD-BRL'
     request = requests.get(url)
     response = request.json()
